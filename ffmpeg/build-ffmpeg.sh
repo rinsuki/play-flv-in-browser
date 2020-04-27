60680fd3dec43147e714c0fea8a6e5a37c156e8e
rinsuki <428rinsuki+git@gmail.com>
2020-04-28 00:16:30 +0900

    build-ffmpeg.sh: 改行を1個追加

--- ffmpeg/build-ffmpeg.sh
+++ ffmpeg/build-ffmpeg.sh
@@ -4,7 +4,8 @@ git clone -b n4.2.1 https://github.com/ffmpeg/ffmpeg src
 cd src
 
 emconfigure ./configure \
-	--cc=emcc --ar=emar --ranlib=emranlib --prefix=$(pwd)/../built --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic --disable-stripping --enable-shared --disable-programs --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --disable-debug --disable-xlib --disable-zlib --disable-sdl2 --disable-iconv --disable-everything --enable-protocol=file \
+	--cc=emcc --ar=emar --ranlib=emranlib --prefix=$(pwd)/../built --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
+	--disable-stripping --enable-shared --disable-programs --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --disable-debug --disable-xlib --disable-zlib --disable-sdl2 --disable-iconv --disable-everything --enable-protocol=file \
 	--enable-decoder=vp6f --enable-decoder=flv --enable-decoder=h264 \
 	--enable-decoder=mp3 --enable-decoder=aac \
     --enable-demuxer=flv
