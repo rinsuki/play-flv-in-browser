783d39ee99bec5249d415b2286d0693d3589a26e
rinsuki <428rinsuki+git@gmail.com>
2020-04-27 19:23:37 +0900

    av_dump_formatでちゃんと動画ファイルを認識できてるか確認するところまで書いた

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+extern "C"
+{
+#include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
 int main(int argc, const char **argv)
 {
-    std::cout << "Hello, world!" << std::endl;
+    av_register_all();
+    AVFormatContext *formatContext = nullptr;
+    if (avformat_open_input(&formatContext, "/input.flv", nullptr, nullptr) != 0)
+    {
+        std::cerr << "avformat_open_input failed..." << std::endl;
+        return 1;
+    }
+    if (avformat_find_stream_info(formatContext, nullptr) < 0)
+    {
+        std::cerr << "avformat_find_stream_info failed..." << std::endl;
+        return 2;
+    }
+    av_dump_format(formatContext, 0, "/input.flv", false);
     return 0;
 }
