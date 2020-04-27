7339400ae1cb67c6e27d43c67a637b425c095b65
rinsuki <428rinsuki+git@gmail.com>
2020-04-27 18:26:37 +0900

    Hello, world! from emscripten

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import wasm from "../wasm/build/wasm-module.js"
+
+// Parcel に wasm をロードさせずコピーだけさせてパスを取る方法がこれしか思い付かなかった
+const wasmPath = document.querySelector<HTMLLinkElement>(`link[data-wasm-file]`).href
+
+console.log(wasm({
+    locateFile: () => wasmPath,
+}))
