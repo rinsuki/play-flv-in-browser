57eed86b513ed46d6ef9c01a17e2beb6e17720e1
rinsuki <428rinsuki+git@gmail.com>
2020-04-27 18:46:19 +0900

    target.flv をwasm内FSにロードさせるように
    別途 src/target.flv にFLVを置く必要があるので注意

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import wasm from "../wasm/build/wasm-module.js"
 
 // Parcel に wasm をロードさせずコピーだけさせてパスを取る方法がこれしか思い付かなかった
 const wasmPath = document.querySelector<HTMLLinkElement>(`link[data-wasm-file]`).href
+const flvPath = document.querySelector<HTMLLinkElement>(`link[data-flv-file]`).href
 
-console.log(wasm({
-    locateFile: () => wasmPath,
-}))
+fetch(flvPath).then(flv => flv.arrayBuffer()).then(flv => {
+    const module = wasm({
+        locateFile: () => wasmPath,
+        // noInitialRun: true,
+        preRun: [() => {
+            module.FS_createDataFile("/", "input.flv", new Uint8Array(flv), true, false, false)
+        }]
+    })
+    window.module = module
+    console.log("yay")
+})
