74198fbfa04ef6f8b7a4c5c684cebe4d73155883
rinsuki <428rinsuki+git@gmail.com>
2021-10-18 12:30:47 +0000

    add mpeg2video/mpegts

--- ffmpeg/build-ffmpeg.sh
+++ ffmpeg/build-ffmpeg.sh
@@ -7,9 +7,10 @@ cd src
 emconfigure ./configure \
 	--cc=emcc --ar=emar --ranlib=emranlib --prefix=$(pwd)/../built --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic \
 	--disable-stripping --enable-shared --disable-programs --disable-asm --disable-doc --disable-devices --disable-pthreads --disable-w32threads --disable-network --disable-debug --disable-xlib --disable-zlib --disable-sdl2 --disable-iconv --disable-everything --enable-protocol=file \
-	--enable-decoder=vp6f --enable-decoder=flv --enable-decoder=h264 \
+	--enable-bsf=aac_adtstoasc \
+	--enable-decoder=mpeg2video --enable-decoder=h264 \
 	--enable-decoder=mp3 --enable-decoder=aac \
-    --enable-demuxer=flv --enable-demuxer=mov \
+    --enable-demuxer=flv --enable-demuxer=mov --enable-demuxer=mpegts \
 	--enable-muxer=mp3 --enable-muxer=mp4
 
 emmake make -j
